c25cc2de17dbfa47f6ed8b81a9e9ef1978cbb90f
Pedro Louzada <pelulouzada@gmail.com>
2022-11-20 17:32:51 -0300

    Send notification

--- src/componentes/NotificationButton/index.tsx
+++ src/componentes/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
 import './NotificationButton.css'
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
+import axios from 'axios';
 
-const NotificationButton = () => {
+type Props = {
+    saleId: number;
+}
+
+function hadleClick(id:number){
+     axios(`${BASE_URL}/sale/${id}/notification`)
+        .then(response => {
+        console.log('Sucesso')
+     })
+}
+
+const NotificationButton = ( {saleId} : Props) => {
     return(
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => hadleClick(saleId)}>
             <img src={icon} alt="Notificar" />
     </div>
     )
